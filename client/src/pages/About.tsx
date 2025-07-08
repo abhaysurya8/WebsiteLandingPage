e0e009547062421fe4407646ff97b31604288926
abhaysurya8 <abhaysuryai813@gmail.com>
2025-07-08 12:24:09 +0530

    Adjust About page image: move object position to 'center top' to show full face

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -12,7 +12,7 @@ const About = () => {
                 src="/Images/back4.jpg"
                 alt="Ar. Shivangi Shivakumar"
                 className="w-80 h-80 rounded-full object-cover shadow-lg hover-scale"
-                style={{ objectPosition: 'center 30%', objectFit: 'cover' }}
+                style={{ objectPosition: 'center top', objectFit: 'cover' }}
               />
             </div>
           </div>
